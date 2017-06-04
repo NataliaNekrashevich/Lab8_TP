ff2e945abbf1d9d09c2279535502522fe2278cb6
Natalia <natalianekrashevich@gmail.com>
2017-06-04 14:43:48 +0300

    v1

---
+++ Lab8_1_1/Lab8_1_1/Record.m
@@ -0,0 +1,19 @@
+//
+//  Record.m
+//  Lab8_1_1
+//
+//  Created by Admin on 28.05.17.
+//  Copyright (c) 2017 admin. All rights reserved.
+//
+
+#import "Record.h"
+
+
+@implementation Record
+
+@dynamic aviaCompany;
+@dynamic cityFrom;
+@dynamic cityTo;
+@dynamic price;
+
+@end
---
+++ Lab8_2/Lab8_2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lab8_2
+//
+//  Created by Admin on 28.05.17.
+//  Copyright (c) 2017 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
